1624c0d66770b1b90e72a62ab5a7f45eb4d2d2f9
Nasrolla <Nasrolla.hassani@gmail.com>
2024-11-04 23:35:27 +0100

    Everything is done, animation left.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const input = document.querySelector(".input");
 const addBtn = document.querySelector(".addBtn");
-const errorPromp = document.querySelector(".error");
+const errorText = document.querySelector(".errorOff");
 const listHolder = document.querySelector(".listHolder");
-// listen for klick on addBtn
+// listen to addBtn for any click.
 addBtn.addEventListener("click", ()=>{
-    console.log("addBtn is clicked")
-    createList();
+    //if the input is empty call the errorPromp to be on. else call the create list functon.
+    if (input.value !== ""){
+        //call error function with a parameter to switch of if its on already.
+        errorPromp("Off");
+        //create new list with input from user.
+        createList();
+        //call the function to clear the input.
+        clearInput();
+    } else {
+        //calling the error function with a condition to be on.
+        errorPromp("On");
+        // console.log("input is empty!");
+    }
 });
 
 
@@ -26,5 +37,35 @@ function createList(){
     const deleteBtn = document.createElement("button");
     deleteBtn.className = "fa-regular fa-trash-can fa-sm";
     listItem.appendChild(deleteBtn);
+    //Delete items from the list
+    deleteBtn.addEventListener("click", ()=>{
+        listHolder.removeChild(listItem);
+    });
+    //mark is done in the list.
+    text.addEventListener("click", ()=>{
+        text.classList.toggle("markAsDone");
+    });
 
 };
+
+//function for error promp when input is empty, this functon is goin to switch the class name of p element depending on condition.
+function errorPromp(status){
+    if (status === "On"){
+        // console.log("error turned On")
+        errorText.classList.add("errorOn");
+        errorText.classList.remove("errorOff");
+    } else if (status === "Off"){
+        // console.log("error turned off")
+        errorText.classList.add("errorOff");
+        errorText.classList.remove("errorOn");
+    };
+};
+
+//function to clear the input
+function clearInput(){
+    input.value = "";
+};
+
+//function to delete item from the list
+
+
